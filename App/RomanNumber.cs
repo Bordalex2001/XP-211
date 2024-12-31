b3889ad7339dd18226317632d05753205ca8a4f0
Bordalex2001 <balex0121@gmail.com>
2024-12-31 11:14:26 +0200

    Add project files.

---
+++ App/RomanNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public record RomanNumber(int Value)
+    {
+        public static RomanNumber Parse(string input) => RomanNumberParser.Parse(input);
+
+        public override string ToString() {
+            Dictionary<int, string> ranges = new() 
+            {
+                {1, "I" },
+                {4, "IV" },
+                {5, "V" },
+                {9, "IX" },
+                {10, "X" },
+            };
+            int n = Value;
+            StringBuilder sb = new();
+            foreach(int range  in ranges.Keys.OrderByDescending(k => k)) {
+                while (n >= range) {
+                    n -= range;
+                    sb.Append(ranges[range]);
+                }
+            }
+            return sb.ToString();
+        }
+
+        // Завдання: створити метод додавання, що проходить тест
+        public RomanNumber Plus(RomanNumber second) => this with { Value = Value + second.Value };
+    }
+}
